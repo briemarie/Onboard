387fd0ccbc7d88115c0821ab2cf1044d150cc919
briemarie <thebriemarie@gmail.com>
2015-07-16 11:16:10 -0700

    Remove Sinatra framework, leaving raw html/css/js

--- README.md
+++
@@ -1,1 +0,0 @@
-# Onboard Videos page for Happy Co.
--- spec/controller/index_controller_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe "index controller" do
-	it "should return a 200 status" do
-		get '/'
-		expect(last_response).to be_ok
-	end
-end
